d7e20d3e4b3199672bb1b9976ca38a20b3e5f228
PaduanAluno <88555950+PaduanAluno@users.noreply.github.com>
2021-08-06 15:34:11 -0300

    Initial commit

---
+++ projeto-apeperia-b2501c63b2ebdfd8226473a6c8ffa8777e559efe/css/Untitled-1.js
@@ -0,0 +1,32 @@
+
+var valor1 = 10
+var valor2 = 20
+var valor3 = 30
+
+var maior = 0
+var menor = 0
+
+if(valor2 > valor1){
+  maior = valor2
+} else if(valor3 > maior){
+    maior = valor3
+}
+console.log(maior)
+    
+
+/*else if(valor3 > maior){
+  maior = valor3
+}else if(valor2 < valor1){
+  menor = valor2
+}else if(valor3 < menor){
+  menor = valor3
+}*/
+
+
+console.log("Maior numero é: " +maior)
+console.log("Menor numero é: " +menor)
+
+
+/*parseInt(prompt("De o valor para o 1 numero"))*/
+/*parseInt(prompt("De o valor para o 2 numero"))*/
+/*parseInt(prompt("De o valor para o 3 numero"))*/
